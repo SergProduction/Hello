77a09e2497b83ce86fc5cd1ad6e8353f9cfeeea1
SergProduction <sdweb@ro.ru>
2016-04-07 00:03:18 +0300

    OK

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var http = require('http');
+var url = require('url');
+var route = require('./route')
 
-var server = new http.Server();
+
+
+
+var server = new http.Server(function(req, res){
+
+  console.log(req.method, req.url);
+  var urlParsed = url.parse(req.url, true);
+  if (urlParsed.query.uri == 'ya.ru') {
+    res.writeHead(200, "Успех"{"Content-Type": "text/plain"});
+    res.end("Hello");
+  }else{
+    res.statusCode = 404;
+    res.end("Page not faund");
+  }
+  console.log(urlParsed);
+
+})
 
 server.listen(3000, 'localhost');
-
-server.on('request', function(req, res){
-  res.end("Hello");
-})
